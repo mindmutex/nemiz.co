c820e1d193b148041d6ebb02486a86cd40aaf6c3
Ivars Veksins <ivarsv@gmail.com>
2014-12-15 20:57:42 +0200

    simplified mysql initialization script - all initial data should be inserted manually.

--- .ebextensions/bin/configure-mysql.sh
+++ .ebextensions/bin/configure-mysql.sh
@@ -5,11 +5,6 @@ echo `pwd` >> /tmp/x
 MYSQL_STATUS=`/sbin/service mysqld status | grep stopped | wc -l`
 if [ "$MYSQL_STATUS" = "1" ]; then
 	/sbin/service mysqld start
-
-	echo "create database symfony" | /usr/bin/mysql -u root
-
-	
-	/usr/bin/mysql -u root symfony < .ebextensions/sql/init.sql
 	/usr/bin/mysqladmin -u root password "$SYMFONY__DATABASE__PASSWORD"
 fi
 
